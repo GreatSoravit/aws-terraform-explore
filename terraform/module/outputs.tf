2de4442fad6060d14b1f9353e2df273c8239b87c
Soravit Wetprasit <103516541+GreatSoravit@users.noreply.github.com>
2025-07-22 22:27:02 +0700

    Create outputs.tf
    outputs terraform

---
+++ terraform/module/outputs.tf
@@ -0,0 +1,14 @@
+output "cluster_name" {
+  description = "The name of the EKS cluster."
+  value       = module.eks.cluster_name
+}
+
+output "s3_bucket_name" {
+  description = "The name of the S3 bucket."
+  value       = aws_s3_bucket.application_bucket.id
+}
+
+output "configure_kubectl" {
+  description = "Run this command to configure kubectl to connect to the EKS cluster."
+  value       = "aws eks update-kubeconfig --region ${module.eks.cluster_region} --name ${module.eks.cluster_name}"
+}
