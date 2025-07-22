b0300d46f68c9fc25dc420b01e35704fc9a26604
Soravit Wetprasit <103516541+GreatSoravit@users.noreply.github.com>
2025-07-23 03:34:44 +0700

    Update main.tf
    remove s3 bucket

--- terraform/module/main.tf
+++ terraform/module/main.tf
@@ -96,60 +96,6 @@ resource "aws_security_group" "ssh_access_sg" {
   }
 }
 
-# Creates a private S3 bucket, useful for storing application data, logs, or backups.
-resource "aws_s3_bucket" "application_bucket" {
-  bucket            = "s3-aws-terraform-explore" # S3 bucket names must be globally unique
-  force_destroy     = true                       # Allows deletion even if files exist (for dev/test buckets)
-  aws_s3_bucket_acl = "private"
-
-  tags = {
-    Name        = "s3-bucket"
-    Project     = "aws-terraform-explore"
-  }
-}
-
-resource "aws_s3_bucket_versioning" "this" {
-  bucket = aws_s3_bucket.application_bucket.id
-  versioning_configuration {
-    status = "Suspended"  # Save costs; versioning costs extra
-  }
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
-  bucket = aws_s3_bucket.application_bucket.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"  # Free SSE encryption
-    }
-  }
-}
-
-resource "aws_s3_bucket_lifecycle_configuration" "this" {
-  bucket = aws_s3_bucket.application_bucket.id
-
-  rule {
-    id     = "transition-to-infrequent-access"
-    status = "Enabled"
-
-    # filter block to apply the rule to all objects.
-    filter {}
-
-    transition {
-      days          = 30
-      storage_class = "STANDARD_IA"  # Cheaper after 30 days
-    }
-
-    expiration {
-      days = 365  # Optional: expire objects after 1 year to save space
-    }
-
-    noncurrent_version_expiration {
-      noncurrent_days = 30
-    }
-  }
-}
-
 # Creates a standalone General Purpose SSD (gp3) EBS volume.
 # This volume can be dynamically provisioned to pods in EKS using the EBS CSI Driver.
 resource "aws_ebs_volume" "database_volume" {
