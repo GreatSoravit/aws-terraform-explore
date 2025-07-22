9cc11b2e2a415b0f7d7b9e68f18eb2d85ac6b619
Soravit Wetprasit <103516541+GreatSoravit@users.noreply.github.com>
2025-07-23 03:37:03 +0700

    Update outputs.tf
    remove s3

--- terraform/module/outputs.tf
+++ terraform/module/outputs.tf
@@ -3,11 +3,6 @@ output "cluster_name" {
   value       = module.eks.cluster_name
 }
 
-output "s3_bucket_name" {
-  description = "The name of the S3 bucket."
-  value       = aws_s3_bucket.application_bucket.id
-}
-
 output "configure_kubectl" {
   description = "Run this command to configure kubectl to connect to the EKS cluster."
   value       = "aws eks update-kubeconfig --region ${data.aws_region.current.name} --name ${module.eks.cluster_name}"
