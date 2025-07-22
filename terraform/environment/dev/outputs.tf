021fa65f0cdec69f249fecdeea7e5eea4dae426d
Soravit Wetprasit <103516541+GreatSoravit@users.noreply.github.com>
2025-07-23 03:37:14 +0700

    Update outputs.tf
    remove s3

--- terraform/environment/dev/outputs.tf
+++ terraform/environment/dev/outputs.tf
@@ -2,10 +2,6 @@ output "cluster_name" {
   value       = module.dev.cluster_name
 }
 
-output "s3_bucket_name" {
-  value       = module.dev.s3_bucket_name
-}
-
 output "configure_kubectl" {
   value       = module.dev.configure_kubectl
 }
