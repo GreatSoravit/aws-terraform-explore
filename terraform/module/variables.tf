9a3bc48e472aae80af1ec98f44e4dc9b7f4407b9
Soravit Wetprasit <103516541+GreatSoravit@users.noreply.github.com>
2025-07-25 18:33:28 +0700

    Update variables.tf

--- terraform/module/variables.tf
+++ terraform/module/variables.tf
@@ -30,3 +30,8 @@ variable "desired_size" {
   description = "Desired number of instances"
   default     = 2
 }
+
+variable "cluster_version" {
+  description = "The version of the EKS cluster (e.g., 1.29)"
+  type        = string
+}
