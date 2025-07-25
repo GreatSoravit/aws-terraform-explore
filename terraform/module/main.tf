47485dda16f393df4155adfedd1fd56094349d5c
Soravit Wetprasit <103516541+GreatSoravit@users.noreply.github.com>
2025-07-26 03:35:52 +0700

    Update main.tf

--- terraform/module/main.tf
+++ terraform/module/main.tf
@@ -21,12 +21,12 @@ module "vpc" {
 
   # Tags required by Kubernetes for service discovery (e.g., for Load Balancers).
   public_subnet_tags = {
-    "kubernetes.io/cluster/main-eks-cluster" = "shared"
+    "kubernetes.io/cluster/${var.environment.name}-eks-cluster" = "shared"
     "kubernetes.io/role/elb"                 = "1"
   }
 
   private_subnet_tags = {
-    "kubernetes.io/cluster/main-eks-cluster" = "shared"
+    "kubernetes.io/cluster/${var.environment.name}-eks-cluster" = "shared"
     "kubernetes.io/role/internal-elb"        = "1"
   }
 }
