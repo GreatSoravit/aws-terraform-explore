704ca3e81b77ae41f2bb934695552ab0234b08f7
Soravit Wetprasit <103516541+GreatSoravit@users.noreply.github.com>
2025-07-26 22:42:05 +0700

    update

--- terraform/environment/dev/main.tf
+++ terraform/environment/dev/main.tf
@@ -13,7 +13,7 @@ provider "kubernetes" {
 }
 
 provider "helm" {
-  kubernetes ช {
+  kubernetes = {
     host                   = module.dev.cluster_endpoint
     cluster_ca_certificate = base64decode(module.dev.cluster_certificate_authority_data)
     token                  = data.aws_eks_cluster_auth.this.token
