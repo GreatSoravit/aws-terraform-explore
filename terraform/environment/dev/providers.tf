67fe9177f755ece687de8957506b179c749b42f6
Soravit Wetprasit <103516541+GreatSoravit@users.noreply.github.com>
2025-07-26 16:37:46 +0700

    test2

--- terraform/environment/dev/providers.tf
+++ terraform/environment/dev/providers.tf
@@ -19,28 +19,26 @@ provider "aws" {
   region = "ap-southeast-7" 
 }
 
-data "aws_eks_cluster" "eks" {
+data "aws_eks_cluster" "cluster" {
   name = module.eks.cluster_name
   depends_on = [module.eks]
 }
 
-data "aws_eks_cluster_auth" "eks" {
+data "aws_eks_cluster_auth" "cluster" {
   name = module.eks.cluster_name
   depends_on = [module.eks]
 }
 
 provider "kubernetes" {
-  alias                  = "eks"
-  host                   = data.aws_eks_cluster.this.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.this.token
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
 }
 
 provider "helm" {
-  alias = "eks"
   kubernetes {
-    host                   = data.aws_eks_cluster.this.endpoint
-    cluster_ca_certificate = base64decode(data.aws_eks_cluster.this.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.this.token
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority[0].data)
+    token                  = data.aws_eks_cluster_auth.cluster.token
   }
 }
