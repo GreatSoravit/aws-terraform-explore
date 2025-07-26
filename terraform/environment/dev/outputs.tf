7ccfd4419bf9b1e65568116919fad60c56abedc6
Soravit Wetprasit <103516541+GreatSoravit@users.noreply.github.com>
2025-07-26 16:47:10 +0700

    update output and provider for dev

--- terraform/environment/dev/outputs.tf
+++ terraform/environment/dev/outputs.tf
@@ -2,6 +2,14 @@ output "cluster_name" {
   value       = module.dev.cluster_name
 }
 
+output "cluster_endpoint" {
+  value = module.eks.cluster_endpoint
+}
+
+output "cluster_certificate_authority_data" {
+  value = module.eks.cluster_certificate_authority_data
+}
+
 output "configure_kubectl" {
   value       = module.dev.configure_kubectl
 }
