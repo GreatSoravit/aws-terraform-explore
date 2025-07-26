827e42861ab6c3475761b48e60d2ca86031d0432
Soravit Wetprasit <103516541+GreatSoravit@users.noreply.github.com>
2025-07-26 15:17:07 +0700

    bracket missing

--- terraform/environment/dev/providers.tf
+++ terraform/environment/dev/providers.tf
@@ -30,3 +30,4 @@ provider "helm" {
     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
     token                  = data.aws_eks_cluster_auth.this.token
   }
+}
