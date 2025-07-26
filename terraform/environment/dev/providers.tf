cfa4d7725debf49715497be2b792fae15a11634c
Soravit Wetprasit <103516541+GreatSoravit@users.noreply.github.com>
2025-07-26 14:54:43 +0700

    add provider on k8s

--- terraform/environment/dev/providers.tf
+++ terraform/environment/dev/providers.tf
@@ -10,3 +10,23 @@ terraform {
 provider "aws" {
   region = "ap-southeast-7" # Thailand
 }
+
+# This data source gets a temporary authentication token for the EKS cluster.
+data "aws_eks_cluster_auth" "this" {
+  name = module.eks.cluster_id
+}
+
+# Configure the Kubernetes provider to connect to the EKS cluster.
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.this.token
+}
+
+# Configure the Helm provider to connect to the EKS cluster.
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+    token                  = data.aws_eks_cluster_auth.this.token
+  }
