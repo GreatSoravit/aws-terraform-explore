510d6f055b52b0fb8235d48c6f5b039c87dddf44
Soravit Wetprasit <103516541+GreatSoravit@users.noreply.github.com>
2025-07-29 18:28:18 +0700

    Update main.tf

--- terraform/module/main.tf
+++ terraform/module/main.tf
@@ -220,7 +220,7 @@ resource "aws_launch_template" "eks_nodes" {
   instance_type = var.instance_type
   
   #vpc_security_group_ids = [
-    aws_security_group.eks_cluster_sg.id
+    #aws_security_group.eks_cluster_sg.id
     # module.eks.cluster_primary_security_group_id #,
     # aws_security_group.ssh_access_sg.id           # custom rule for SSH
   #]
