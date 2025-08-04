d01a57cefa9767f6d583812cbf7a53ec4f17c033
unknown <soravitwetprasit@gmail.com>
2025-08-04 18:52:12 +0700

    update backend.tf in dev and qa env

--- terraform/environment/dev/backend.tf
+++ terraform/environment/dev/backend.tf
@@ -7,4 +7,10 @@ terraform {
       name = "aws-terraform-explore"
     }
   }
+  backend "s3" {
+    bucket         = "terraform-explore-project-initials-tfstate"
+    key            = "dev/eks/terraform.tfstate"
+    region         = "ap-southeast-7"
+    dynamodb_table = "terraform-explore-project-lock"
+  }
 }
--- terraform/environment/qa/backend.tf
+++ terraform/environment/qa/backend.tf
@@ -7,4 +7,10 @@ terraform {
       name = "qa-aws-terraform-explore"
     }
   }
+  backend "s3" {
+    bucket         = "terraform-explore-project-initials-tfstate"
+    key            = "qa/eks/terraform.tfstate"
+    region         = "ap-southeast-7"
+    dynamodb_table = "terraform-explore-project-lock"
+  }
 }
